7b5ac0fb17f23a120a5b2f32ec7ffc31a59e2875
Igor <igorretesz@gmail.com>
2018-10-21 20:14:48 +0100

    Web server
    Basic python HTTPd server

--- web-client/serve.py
+++ web-client/serve.py
@@ -10,3 +10,33 @@
 #|    | __      |  |
 #|    ||  |     |  |
 #\____||__|_____|__|
+#
+#Import any important modules
+from consoleTools import consoleDisplay
+import http.server
+import socketserver
+import socket
+
+#Any variables that need to be set beforehand
+PORT = 8080
+
+#Starting the server
+consoleDisplay.log('i','Setup: Preparing to serve webserver on {}'.format(PORT),False)
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+try:#Display hostname
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    consoleDisplay.log('i',"\nHostname: {}\nIP: {}".format(host_name,host_ip),False)
+except:
+    consoleDisplay.log('e',"Couldn't get Hostname or IP")
+
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    consoleDisplay.log('e',"KEYBOARD INTERRUPT. EXCEPT\n")
+    pass
+
+consoleDisplay.log('w',"Server is about to go down")
+httpd.server_close()
+consoleDisplay.log('i',"Server is no longer serving")
