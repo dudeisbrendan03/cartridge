b54fd77daa796a363f58effa4a2321abebfa7aa8
Brendan <jbrendan70@outlook.com>
2018-10-22 15:38:24 +0100

    Force start parameter (server)

--- server/start.py
+++ server/start.py
@@ -1,15 +1,30 @@
 #This script will start Cartridge server normally (with any parameters)
 
 #Import modules that are required most of the time/at all times
-from sys import argv
-from os import name as opname
-from consoleTools import consoleDisplay
-import subprocess
+import sys#needed for arguments
+from os import name as opname#needed to identify os
+from consoleTools import consoleDisplay#needed for logging
+import subprocess#needed for execution
+
+argamnt=len(sys.argv)
+arglist=sys.argv
+print(arglist)
+del arglist[0]
+print(str(arglist))
+try:
+    if arglist[0] == "--force":
+        cont=True
+except IndexError: cont=False
+print(str(sys.argv))
 
 if opname == 'posix':
     print("Not supported on linux machines yet.\nTo launch anyway, run with the parameter --force")
-    consoleDisplay.log('e','Killed before launch, running posix. Force launch using --force parameter')
-    exit()
+    if cont == False:
+        try:
+            consoleDisplay.log('e','Killed before launch, running posix. Force launch using --force parameter')
+        except ImportError:
+            print("Missing dependencies.")
+        exit()
 
 from datetime import datetime
 consoleDisplay.log('i','Server start attempt at {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
